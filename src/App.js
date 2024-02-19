afffe8401c75099bde2e7998eb23d006e551995e
saadashraf <saadjewani888@gmail.com>
2024-02-19 18:03:39 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import image1 from "./img/image1.png";
+import image2 from "./img/image2.png";
+import image3 from "./img/product9.png";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [image, setImage] = useState(1);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <img
+          src={image1}
+          className={` App-logo ${
+            image === 1 ? "App-logo-arr" : "App-logo-dep"
+          }  `}
+          alt="logo"
+        />
+        <img
+          src={image2}
+          className={` App-logo ${
+            image === 2 ? "App-logo-arr" : "App-logo-dep"
+          } `}
+          alt="logo"
+        />
+        <img
+          src={image3}
+          style={{}}
+          className={` App-logo ${
+            image === 3 ? "App-logo-arr" : "App-logo-dep"
+          } `}
+          alt="logo"
+        />
+
+        <button
+          onClick={() => {
+            if (image < 3) {
+              setImage(image + 1);
+            } else {
+              setImage(1);
+            }
+          }}
         >
-          Learn React
-        </a>
+          Click Me
+        </button>
       </header>
     </div>
   );
